b1efe5e1764dedb55d5c1e833bfd4ba3846f0c18
vamshi <vamshi@wooplr.com>
2016-07-12 09:10:28 +0530

    first code commit

---
+++ Resources/ComputeReport.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from scrapy.utils.project import get_project_settings
+
+settings = get_project_settings()
+client = MongoClient(settings.get("MONGODB_HOST"),settings.getint("MONGODB_PORT"))
+db = client[settings.get('MONGODB_DATABASE')]
+
+stocksPrice={}
+for stk in db['stockDetails'].find({}):
+	stocksPrice[stk['name']] = stk['price']	
+
+
+amountMap={}
+for stk in db['investments'].aggregate([{"$group":{"_id":"$stock","totalAmount":{"$sum":{"$multiply":["$price","$units"]}}}}]):
+	amountMap[stk['_id']] = stk['totalAmount']
+
+unitsMap = {}
+for stk in db['investments'].aggregate([{"$group":{"_id":"$stock","totalUnits":{"$sum":"$units"}}}]):
+	unitsMap[stk['_id']] = stk['totalUnits']
+
+print "StockName, CurrentPrice, AveragePrice, Percentage"
+for name,totalAmount in amountMap.items():
+	avgPrice = (totalAmount / unitsMap[name])
+	# percentage = ( (stocksPrice[name] - avgPrice) / avgPrice ) * 100
+	# print name, stocksPrice[name], avgPrice, percentage, "%"
+	print name, stocksPrice[name], avgPrice
